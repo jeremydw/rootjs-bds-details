5d4938096fc4a23b22d673504537db64e1c90bd1
jeremydw <jeremydw@gmail.com>
2023-09-14 12:19:14 -0700

    2 files modified

--- elements/bds-details/bds-details.ts
+++ elements/bds-details/bds-details.ts
@@ -4,11 +4,32 @@ import { classMap } from 'lit/directives/class-map.js';
 import styles from './bds-details.scss?inline';
 
 @customElement('bds-details')
-class MyElement extends LitElement {
+class BdsDetails extends LitElement {
   static styles = unsafeCSS(styles);
 
   @property({ type: Boolean, reflect: true })
   open = false;
+
+  /**
+   * Implements an accordion effect where other siblings close when
+   * this element is opened.
+   */
+  updateSiblings() {
+    const siblings = this.parentElement.querySelectorAll('bds-details');
+    siblings.forEach((sibling: BdsDetails) => {
+      if (sibling === this || !this.open) {
+        return;
+      }
+      sibling.open = false;
+    });
+  }
+
+  attributeChangedCallback(name, old, newVal) {
+    super.attributeChangedCallback(name, old, newVal);
+    if (name === 'open') {
+      this.updateSiblings();
+    }
+  }
 
   render() {
     return html`
--- routes/index.tsx
+++ routes/index.tsx
@@ -7,6 +7,24 @@ export function Page() {
         <p>Lorem ipsum dolor sit amet</p>
         <p>Lorem ipsum dolor sit amet</p>
       </bds-details>
+      <bds-details>
+        <p>Lorem ipsum dolor sit amet</p>
+        <p>
+          <img
+            src="https://images.ctfassets.net/sfnkq8lmu5d7/1NaIFGyBn0qwXYlNaCJSEl/ad59ce5eefa3c2322b696778185cc749/2021_0825_Kitten_Health.jpg?w=1000&h=750&fl=progressive&q=70&fm=jpg"
+            style="max-width: 100%"
+          />
+        </p>
+        <p>Lorem ipsum dolor sit amet</p>
+        <p>Lorem ipsum dolor sit amet</p>
+        <p>Lorem ipsum dolor sit amet</p>
+      </bds-details>
+      <bds-details>
+        <p>Lorem ipsum dolor sit amet</p>
+        <p>Lorem ipsum dolor sit amet</p>
+        <p>Lorem ipsum dolor sit amet</p>
+        <p>Lorem ipsum dolor sit amet</p>
+      </bds-details>
     </>
   );
 }
